c4e541f72a5a2c3e2a68b5004b33f3313112649c
JuanHuamaniVasquez <jhuamaniv@ulasalle.edu.pe>
2025-04-09 18:59:37 -0500

    Add files via upload

---
+++ ejemplo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+class Laboratorio
+{
+	int num;
+};
+
+class Practica
+{
+	int a; 
+	Laboratorio lab;
+
+	public:
+	operator Laboratorio () { return lab; }
+	operator int () { return a;}
+
+};
+void funcion (int a) {
+	std::cout << "funcion (int) executada";
+}
+void funcion1 (Laboratorio la){
+	std::cout << "funcion (Laboratorio) executada";
+}
+int main()
+{
+	Practica p;
+	funcion(p);
+	funcion1(p);
+	return 0;
+}
