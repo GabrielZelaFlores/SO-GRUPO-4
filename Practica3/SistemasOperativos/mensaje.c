8283da1cb297f08c790b0eb379afae88926c94e8
MelvinYabar <myabarc@ulasalle.edu.pe>
2025-04-09 19:11:55 -0500

    Add files via upload

---
+++ Practica3/SistemasOperativos/mensaje.c
@@ -0,0 +1,6 @@
+#include "salida_alt.h"
+int main()
+{
+	muestra ("Muestra este bonito mensaje por la salida estandar ");
+	return 0;
+}
---
+++ Practica3/SistemasOperativos/salida_alt.h
@@ -0,0 +1,4 @@
+#ifndef SALIDA_ALT
+#define SALIDA_ALT 
+void muestra (char*);
+#endif
